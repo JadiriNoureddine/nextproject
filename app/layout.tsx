71ad0e97e0afe86b70844635f7fa6685d3331178
JadiriNoureddine <noureddine.jadiri.etu22@ensem.ac.ma>
2024-07-09 09:25:03 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import '@/app/ui/global.css';
+import { inter, lusitana } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
